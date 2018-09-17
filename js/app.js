aea69d2f318d0c4ebe55e6652a033d568ce68af6
omolayole <omolayoleogunyemi@gmail.com>
2018-09-17 13:54:52 +0100

    add the createGameBoard function to the app.js file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
+const symbols = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o",
+"fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf",
+"fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
+const cardsContainer = document.querySelector('.deck');
 
 /*
  * Display the cards on the page
@@ -25,14 +29,20 @@ function shuffle(array) {
     return array;
 }
 
+//create the game board function
+function createGameBoard() {
+    //shuffle the card
+    const shuffledCards = shuffle(symbols);
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    //create card
+    for (let i = 0; i < shuffledCards.length; i++) {
+        const card = document.createElement('li');
+        card.classList.add("card");
+        card.innerHTML = `<i class="${shuffledCards[i]}"></i>`;
+        cardsContainer.appendChild(card);
+
+        //call click function on the card
+        click(card);
+    }
+}
+
